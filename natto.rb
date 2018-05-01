9318effe3eec9098a7906613e82ba1df7c571ad1
ciloholic <ciloholic0119@gmail.com>
2018-05-01 16:01:21 +0900

    URLの削除を追加

--- natto.rb
+++ natto.rb
@@ -2,6 +2,7 @@ require 'natto'
 require 'open-uri'
 require 'nokogiri'
 require 'sanitize'
+require 'uri'
 require 'pp'
 
 url = 'XXXXXXXXXX'
@@ -9,10 +10,12 @@ doc = Nokogiri::HTML(open(url)) do |config|
   config.noblanks
 end
 
+# scriptを削除
 doc.search('script').each do |script|
   script.content = ''
 end
 
+# styleを削除
 doc.search('style').each do |style|
   style.content = ''
 end
@@ -23,11 +26,11 @@ path = `echo \`mecab-config --dicdir\`"/mecab-ipadic-neologd"`
 natto = Natto::MeCab.new("-d #{path.chomp}")
 wordList = {}
 doc.css('body').each do |elm|
-  text = Sanitize.clean(elm.content)
+  text = Sanitize.clean(elm.content).gsub(URI.regexp, '')
   natto.parse(text) do |n|
     wordList[n.surface] ? wordList[n.surface] += 1 : wordList[n.surface] = 1 if n.feature.match('名詞')
   end
 end
 
 # 降順ソート後、先頭10件を表示
-pp wordList.sort_by{|_, v| -v}.take(30)
+pp wordList.sort_by{|_, v| -v}.take(10)
